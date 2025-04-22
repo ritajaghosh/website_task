cce26f32b9768060ef2958096c7b2c3b02406ec4
Ritaja Ghosh <ritajaghosh@gmail.com>
2025-04-22 20:50:52 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+        <div class="col-md-6">
+            <div class="card shadow">
+            <div class="card-body">
+                <h3 class="text-center mb-4">Register</h3>
+                <form>
+                
+                <div class="mb-3">
+                    <label for="email" class="form-label">Title</label>
+                    <input type="title" class="form-control" id="email" placeholder="Enter title" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Entry</label>
+                    <input type="Entry" class="form-control" id="entry" placeholder="Entry" required>
+                </div>
+                
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+                </form>
+                <p class="text-center mt-3">Already have an account? <a href="#">Login</a></p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
