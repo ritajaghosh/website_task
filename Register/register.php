fbd12bd9d5b3c56ba54c912743669836ad51db64
Ritaja Ghosh <ritajaghosh@gmail.com>
2025-04-29 15:27:38 +0530

    added data on server site

--- Register/register.php
+++ Register/register.php
@@ -1,29 +1,27 @@
 <?php
-    print_r($_POST);
     $name=$_POST['name'];
     $dob=$_POST['dob'];
     $cname=$_POST['cname'];
-    $schname=$_POST['$schname'];
+    $schname=$_POST['schname'];
     $email=$_POST['email'];
-    $username=$_POST['$username'];
+    $username=$_POST['username'];
     $password=$_POST['password'];
     $rpassword=$_POST['rpassword'];
 
-   if (isset($name , $dob , $cname ,$schname ,$email ,$username ,$password ,$rpasssword )){
+   if (isset($name , $dob , $cname ,$schname ,$email ,$username ,$password  )){
     require_once "config.php";
     
       $stmt=$pdo->prepare("insert into users(name,date_of_birth,childhood_name,school_name,email,user_name,user_password)values(?,?,?,?,?,?,?); ");
-      $stmt->execute([$name,$dob,$cname,$schname,$email,$username,$password]);
-      $user=$stmt->fetch(PDO::FETCH_ASSOC);
-      $returnarray=Array();
+      if($stmt->execute([$name,$dob,$cname,$schname,$email,$username,$password])){
+        echo "register";
+    }else{
+        echo "not registered";
+    }
+    //   $returnarray=Array();
       
-      if(empty($user)){
-           
-           $returnarray["status"]= false;
-      }else{
-           
-           $returnarray["status"]= true;
-      }
-      echo json_encode($returnarray);
+      
+    //   echo json_encode($returnarray);
     
+   }else{
+    echo "data not found";
    }
--- Register/registration.php
+++ Register/registration.php
@@ -19,42 +19,42 @@
         <form id="registrationform">
           <div class="mb-3 form-field">
             <label class="form-label">Full Name</label>
-            <input type="text" class="form-control" placeholder="Name..." id="name" required>
+            <input type="text" class="form-control" placeholder="Name..." id="name">
           </div>
           <div class=" form-field mb-3">
             <label class="form-label">Date of Birth</label>
-            <input type="date" class="form-control" placeholder="**********" id="dob" required>
+            <input type="date" class="form-control" placeholder="**********" id="dob">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">What is your childhood name?</label>
-            <input type="name" class="form-control" placeholder="**********" id="cname" required>
+            <input type="text" class="form-control" placeholder="**********" id="cname">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">What is your school name?</label>
-            <input type="nanme" class="form-control" placeholder="**********" id="schname" required>
+            <input type="text" class="form-control" placeholder="**********" id="schname">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">Email</label>
-            <input type="email" class="form-controlname/" placeholder="Email address..." id="email" required>
+            <input type="email" class="form-controlname/" placeholder="Email address..." id="email">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">Username</label>
-            <input type="text" class="form-control" placeholder="Username..." id="username" required>
+            <input type="text" class="form-control" placeholder="Username..." id="username">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">Password</label>
-            <input type="password" class="form-control" placeholder="**********" id="password" required>
+            <input type="password" class="form-control" placeholder="**********" id="password">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label">Repeat Password</label>
-            <input type="password" class="form-control" placeholder="**********" id="rpassword" required>
+            <input type="password" class="form-control" placeholder="**********" id="rpassword">
           </div>
           <div class="mb-3 form-field ">
             <label class="form-label"></label>
-            <input type="password" class="form-control" placeholder="**********" id="conpassword" required>
+            <input type="password" class="form-control" placeholder="**********" id="conpassword">
           </div>
           <div class="form-check mb-3 form-field ">
-            <input class="form-check-input" type="checkbox" required>
+            <input class="form-check-input" type="checkbox">
             <label class="form-check-label">
               I agree to the <a href="#">Terms of User</a>
             </label>
@@ -70,46 +70,48 @@
     </div>
   </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-<script>
-    $('#registrationform').on('submit',function (event) {
-            event.preventDefault();
-            let name=$("#name").val();
-            let dob=$("#dob").val();
-            let cname=$("#cname").val();
-            let schname=$("#schname").val();
-            let email=$("#email").val();
-            let username=$("#username").val();
-            let password=$("#password").val();
-            let repassword=$("#rpassword").val();
-            alert(schname);
+  <script>
+    $('#registrationform').on('submit', function (event) {
+      event.preventDefault();
 
-        $.ajax({
-            type: "post",
-            url: "register.php",
-           
-            data: {
-                name=name,
-                dob=dob,
-                cname=cname,
-                schname=schname,
-                email=email,
-                username=username,
-                password=password,
-                rpassword=rpassword,
+      let name = $("#name").val();
+      let dob = $("#dob").val();
+      let cname = $("#cname").val();
+      let schname = $("#schname").val();
+      let email = $("#email").val();
+      let username = $("#username").val();
+      let password = $("#password").val();
+      let repassword = $("#rpassword").val();
+      console.log(name, dob, cname, schname, email, username, password, repassword);
+      $.ajax({
+        type: "post",
+        url: "register.php",
+        data: {
+          name: name,
+          dob: dob,
+          cname: cname,
+          schname: schname,
+          email: email,
+          username: username,
+          password: password
 
-            },
-            success: function(response)
-            {
-                
-                alert(response);
+        },
+        success: function (response) {
+          alert(response);
+        },
+        error: function (xhr, status, error) {
+          console.error(error);
+        }
 
-            }
-        });    
-       });
-</script>
+      });
+    }
+    )
+    
+  </script>
 </body>
 
 </html>
